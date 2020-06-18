57f347867746b4877c49ddddb73d3a742a2eedea
TheArifCAN <58721687+TheArifCAN@users.noreply.github.com>
2020-06-18 22:41:25 +0300

    gonderme

---
+++ NTP1 Vize Uygulaması/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTP1_Vize_Uygulaması
+{
+    class Program
+    {
+        public static kitap[] Kitabevi { get; private set; }
+
+        static void Main(string[] args)
+        {
+            
+
+            {
+                
+
+
+                Console.WriteLine("Kitap eklemek için1'e\nKitap listesini görmek için 2'ye basınız");
+                byte liste = byte.Parse(Console.ReadLine());
+                if (liste==1)
+                {
+                    Console.WriteLine("Kaç kitap ekleyeceksiniz?");
+                    byte kitapsecimi = byte.Parse(Console.ReadLine());
+                    Kitabevi = new kitap[kitapsecimi];
+                    for (int i = 0; i < kitapsecimi; i++)
+                    {
+                        kitap kitap = new kitap();
+                        Console.WriteLine("Lütfen kitap bililerini ekleyiniz");
+                        Console.WriteLine("Kitap adını giriniz: ");
+                        kitap.Isim = Console.ReadLine();
+                        Console.WriteLine("kitap türünü giriniz");
+                        kitap.Tür = Console.ReadLine();
+                        Console.WriteLine("kitap yazarını giriniz");
+                        kitap.Yazar = Console.ReadLine();
+                        Console.WriteLine("kitap basım yılını giriniz");
+                        kitap.Tarih = new DateTime(Convert.ToInt32(Console.ReadLine()), 1, 1);
+                        Console.WriteLine("------------------------------------------------");
+                        Kitabevi[i] = kitap;
+
+                        
+
+
+
+                        
+
+
+                    }
+                    
+
+                }
+                
+                else if (liste==2)
+                {
+                    kitap.Kitaplar();
+                }
+                else
+                {
+                    Console.WriteLine("Hatalı Giriş");
+                }
+
+                
+
+
+
+            }
+        }
+    }
+    
+}
---
+++ NTP1 Vize Uygulaması/kitap.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+
+namespace NTP1_Vize_Uygulaması
+{
+    public class kitap
+    {
+
+
+
+        
+       public string isim;
+        public string tür;
+        public string yazar;
+        public DateTime tarih
+        {
+            get
+            {
+                return tarih;
+            }
+            set
+            {
+                if
+                    (value.Year <= 2020)
+                {
+                }
+                else
+                {
+                    Console.WriteLine("Yıl 2020'den küçük olmalı");
+                    tarih = new DateTime(Convert.ToInt32(Console.ReadLine()), 1, 1);
+                }
+            }
+
+        }
+
+        public string Isim { get => isim; set => isim = value; }
+        public string Tür { get => tür; set => tür = value; }
+        public string Yazar { get => yazar; set => yazar = value; }
+        public DateTime Tarih { get => tarih; set => tarih = value; }
+
+        private static string Yol = AppDomain.CurrentDomain.BaseDirectory + "kitapliste.txt";
+        public static void Kitaplar()
+        {
+            if (File.Exists(Yol))
+            {
+                string bilgilerioku = File.ReadAllText(Yol);
+                Console.WriteLine(bilgilerioku);
+            }
+        }
+        
+        public static void Yazdır(string isim, string tür,string yazar, DateTime tarih)
+        {
+            string kitaplar = "\nkitap bilgileri \nkitap adı: " + isim + "\nkitap türü: " + tür + "\nkitap yazarı: " + yazar + "\nbasım tarihi: " + tarih ;
+            
+        }
+
+        
+        
+        
+        
+
+
+    }
+      
+    }
